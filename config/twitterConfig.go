b475f8a5b7f26ad4e16767fd93767e69b775eae8
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-02-12 16:32:12 +0530

    add env support

--- config/twitterConfig.go
+++ config/twitterConfig.go
@@ -1,5 +1,10 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 type twitterConfig struct {
   consumerKey string
   consumerSecret string
@@ -7,11 +12,21 @@ type twitterConfig struct {
   accessTokenSecret string
 }
 func GetTwitterConfig() twitterConfig{
+  return getEnvVariables()
+}
+
+
+func getEnvVariables() twitterConfig{
+  viper.SetConfigFile("../.env")
+  err := viper.ReadInConfig()
+  if err!= nil{
+    fmt.Println("error while reading config file", err)
+  }
   tc := twitterConfig{
-  "sdf",
-  "sdf",
-  "sdf",
-  "sdf",
+    consumerKey: viper.Get("CONSUMER_KEY").(string),
+    consumerSecret: viper.Get("CONSUMER_SECRET").(string),
+    accessToken: viper.Get("ACCESS_TOKEN").(string),
+    accessTokenSecret: viper.Get("ACCESS_TOKEN_SECRET").(string),
   }
   return tc
 }
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"friday-go/config"
 )
 func main(){
+  //load env file 
+
+
   //print twitter config
   fmt.Println("this is init file ")
   fmt.Println("twitter config:", config.GetTwitterConfig())
