e8d854564637e6ac059b94a9ddc1968e70e3c89c
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-02-18 12:41:18 +0530

    add auth and retweet functionality

--- config/twitterConfig.go
+++ config/twitterConfig.go
@@ -5,28 +5,28 @@ import (
 
 	"github.com/spf13/viper"
 )
-type twitterConfig struct {
-  consumerKey string
-  consumerSecret string
-  accessToken string 
-  accessTokenSecret string
+type TConfig struct {
+  ConsumerKey string
+  ConsumerSecret string
+  AccessToken string 
+  AccessTokenSecret string
 }
-func GetTwitterConfig() twitterConfig{
+func GetTwitterConfig() TConfig{
   return getEnvVariables()
 }
 
 
-func getEnvVariables() twitterConfig{
+func getEnvVariables() TConfig{
   viper.SetConfigFile(".env")
   err := viper.ReadInConfig()
   if err!= nil{
     fmt.Println("error while reading config file", err)
   }
-  tc := twitterConfig{
-    consumerKey: viper.Get("CONSUMER_KEY").(string),
-    consumerSecret: viper.Get("CONSUMER_SECRET").(string),
-    accessToken: viper.Get("ACCESS_TOKEN").(string),
-    accessTokenSecret: viper.Get("ACCESS_TOKEN_SECRET").(string),
+  tc := TConfig{
+    ConsumerKey: viper.Get("CONSUMER_KEY").(string),
+    ConsumerSecret: viper.Get("CONSUMER_SECRET").(string),
+    AccessToken: viper.Get("ACCESS_TOKEN").(string),
+    AccessTokenSecret: viper.Get("ACCESS_TOKEN_SECRET").(string),
   }
   return tc
 }
--- helper/auth.go
+++ helper/auth.go
@@ -1,1 +1,32 @@
 package helper
+
+import (
+	"flag"
+	"fmt"
+	"friday-go/config"
+	"net/http"
+	twitter "github.com/g8rswimmer/go-twitter/v2"
+)
+
+
+
+type Authorize struct{
+  Token string
+}
+
+func (a Authorize) Add(req  *http.Request){
+  req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", a.Token))
+}
+
+
+func InitAuth(twitterConfig config.TConfig) *twitter.Client{
+  token := flag.String("token", "", twitterConfig.AccessToken)
+  client := &twitter.Client{
+    Authorizer : Authorize {
+      Token: *token,
+    },
+    Client : http.DefaultClient,
+    Host: "https://api.twitter.com",
+  }
+  return client
+}
